829783b95394de9008be911692b434283c6cee8d
alde2616 <alde2616@student.su.se>
2020-10-15 15:25:21 +0200

    sword class update

--- Sword.java
+++ Sword.java
@@ -1,9 +1,9 @@
-public class Sword extends Weapon{
+public class Sword extends Weapon {
     private int attack = 5;
 
     public Sword(String name, int level) {
         super(name, level);
-        attack += 2 * level;
+        attack += 2 * level; //Attack ökar med item level
     }
 
     public int getAttack() {
