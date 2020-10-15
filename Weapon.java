3648a2291cccc204ef1317af6a55344049032683
alde2616 <alde2616@student.su.se>
2020-10-15 15:23:03 +0200

    weapon class update

--- Weapon.java
+++ Weapon.java
@@ -1,12 +1,14 @@
 public abstract class Weapon {
-    private String name;
-    private int level = 1;
+    private final String name;
+    private final int level;
 
-    public Weapon (String name, int level) {
-     this.name = name;
-     if(level < 1 || level > 100)
-         throw new IllegalArgumentException("level: " + level);
-     this.level = level;
+    public Weapon(String name, int level) {
+        if (name.isBlank())
+            throw new IllegalArgumentException("Name: blank"); //Namn för inte vara blank
+        this.name = name;
+        if (level < 1 || level > 100)
+            throw new IllegalArgumentException("level: " + level); //Level för inte vara lägre än 1 eller större än 100
+        this.level = level;
     }
 
     public String getName() {
