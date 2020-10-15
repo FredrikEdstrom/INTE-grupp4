09663ee853c763cac3fefd0fe10e09c9efc0f5d4
alde2616 <alde2616@student.su.se>
2020-10-15 13:57:42 +0200

    weapon test class

---
+++ WeaponTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeaponTest {
+
+    @Test
+    void createWeaponSword() {
+        Sword sword = new Sword("excalibur", 10);
+        assertEquals("excalibur", sword.getName());
+        assertEquals(10, sword.getLevel());
+        assertEquals(25, sword.getAttack());
+    }
+    @Test
+    void createWeaponStaff() {
+        Staff staff = new Staff("darkness staff", 10);
+        assertEquals("darkness staff", staff.getName());
+        assertEquals(10, staff.getLevel());
+        assertEquals(25, staff.getIntelligence());
+    }
+
+    @Test
+    void weaponLevelLowerThanOne() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Sword("excalibur", 0));
+    }
+    @Test
+    void weaponLevelHigherThanOneHundred() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Staff("darkness staff", 101));
+    }
+}
