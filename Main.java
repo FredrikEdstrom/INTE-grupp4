26db7cbd036750f2f520f70a5f396b731fed786d
ph0enix10101 <kolaaaisbest@gmail.com>
2020-10-20 16:34:41 +0200

    Added attack, defense and magic attack method

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Hero player = new Hero();
+		Hero player = new Hero(player);
 		Hero dummy = new Hero(20, 1, 100, 100);
 		player.attackOrder(player, dummy);
 		player.defenseOrder(player, dummy);
