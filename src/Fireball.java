41662510d43f7b76f5949b34f25267e983635e4d
Fredrik Edström <fred8493@student.su.se>
2020-10-24 21:28:22 +0200

    added fireball spell and test

--- src/Fireball.java
+++ src/Fireball.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Fireball {
+public class Fireball {
+    private static final String DAMAGE_TYPE = "Fire";
+    private static final int DEFAULT_DAMAGE = 20;
+    private static final int LEVEL_REQUIREMENT = 1;
+
+    public String getDamageType(){
+        return DAMAGE_TYPE;
+    }
+
+    public int getDefaultDamage(){
+        return DEFAULT_DAMAGE;
+    }
+
+    public int getLevelRequirement(){
+        return LEVEL_REQUIREMENT;
+    }
+
+    public int getModifiedDamage(int intelligence){
+        return DEFAULT_DAMAGE + intelligence * 2;
+    }
 }
--- test/FireballTest.java
+++ test/FireballTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FireballTest {
-  
+
+    @Test
+    void constantValuesSet(){
+        Fireball fireball = new Fireball();
+        assertEquals(20,fireball.getDefaultDamage());
+        assertEquals("Fire",fireball.getDamageType());
+        assertEquals(1,fireball.getLevelRequirement());
+    }
+
+    @Test
+    void damageAffectedByHeroIntelligence(){
+        Fireball fireball = new Fireball();
+        Hero hero = new Hero("hero");
+        assertEquals(40,fireball.getModifiedDamage(hero.getIntelligence()));
+    }
+
 }
